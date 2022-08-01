5a881a50583ed038c298de9c4cbbbfdd4464bf5b
suraj10297 <ssgawade100297@gmail.com>
2022-08-01 14:58:34 +0530

    committed

---
+++ server.js
@@ -0,0 +1,41 @@
+
+var express=require('express');
+var app=express();
+
+
+
+app.get("/",(req, res)=>{
+    res.send(
+            "<h1>Vijay Sales </h1>"
+            +"<hr/>"
+            +"<h3>Smart Devices for sale</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Laptops</li>"
+            +"<li>Mobile Phones</li>"
+            +"<li>Hololgraphic Devices</li>"
+            +"<li>Samrt Watches</li>"
+            +"<li>Gaming Consoles</li>"
+            + "</ol>"
+    );
+});
+
+app.get("/aboutus",(req, res)=>{
+    res.send(
+            "<h1>Room 3 </h1>"
+            +"<hr/>"
+            +"<h3>Doing ordinary things extraordinarily</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Chief Mentor: Ravi Tambade</li>"
+            +"<li>Director: Shubhangi Tambade</li>"
+            +"<li>Subject Matter Expert: Rohit Gore</li>"
+            + "</ol>"
+    );
+});
+var server=app.listen(8080);
+console.log("Vijay Sales Online shopping running on port 8080");
+
+
+
+
